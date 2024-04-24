44b5da5921927f963fcedc6296ad034acaa366a2
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-04-25 02:47:25 +0300

    Basic dependencies setup with the Text package.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
+
+import Data.Text as T
+import Data.Text.IO as I
+
+testText :: T.Text
+testText = "Text wroks!!1"
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    I.putStrLn testText
