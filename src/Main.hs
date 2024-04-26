1e7e36154b6967b6bd6063e03df73230875e8cb2
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-04-26 03:30:27 +0300

    Cleaned up a redundant qualified import in Main.

--- src/Main.hs
+++ src/Main.hs
@@ -3,18 +3,18 @@
 module Main (main) where
 
 import Data.Text as T
-import Parse as P
+import Parse
 
 main :: IO ()
 main = do
-    print $ P.many P.digit $ T.pack "KEKW"
-    print $ P.many P.digit $ T.pack "1kek"
-    print $ P.many P.digit $ T.pack "1230"
-    print $ P.many P.digit $ T.pack "123kek"
-    print $ P.many P.digit $ T.pack "123"
+    print $ many digit $ T.pack "KEKW"
+    print $ many digit $ T.pack "1kek"
+    print $ many digit $ T.pack "1230"
+    print $ many digit $ T.pack "123kek"
+    print $ many digit $ T.pack "123"
     print $ T.pack "==================="
-    print $ P.number $ T.pack ""
-    print $ P.number $ T.pack "KEKW"
-    print $ P.number $ T.pack "1KEK"
-    print $ P.number $ T.pack "666KEK"
-    print $ P.number $ T.pack "KEK666"
+    print $ number $ T.pack ""
+    print $ number $ T.pack "KEKW"
+    print $ number $ T.pack "1KEK"
+    print $ number $ T.pack "666KEK"
+    print $ number $ T.pack "KEK666"
